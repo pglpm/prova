0b8e3f8cf125cc75cd671410cc4e91cbf17025da
pglpm <pgl@portamana.org>
2026-07-07 17:18:55 +0200

    update par() handling

--- R/util_plotfunctions.R
+++ R/util_plotfunctions.R
@@ -214,6 +214,9 @@ flexiplot <- function(
         graphics::axis(2, at = yat, labels = ydomain, tick = !grid,
             col = 'black', lwd = 1, lty = 1, ...)
         if(grid){
+            ## Save and restore user's par()
+            oldpar <- par(no.readonly = TRUE)
+            on.exit(par(oldpar))
             if(!is.null('xaxp')){ par(xaxp = xaxp) }
             if(!is.null('yaxp')){ par(yaxp = yaxp) }
             graphics::grid(nx = NULL, ny = NULL, lty = 1, col = '#BBBBBB80')
