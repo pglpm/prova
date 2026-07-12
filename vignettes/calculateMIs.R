d6b04ef15e61854412051853ded810f29b4ba7b9
pglpm <pgl@portamana.org>
2026-07-13 00:01:19 +0200

    update mutualinfo vignette

---
+++ vignettes/calculateMIs.R
@@ -0,0 +1,52 @@
+library('prova')
+
+learnt <- 'learntall.rds'
+
+parallel <- 12
+
+temp <- mutualinfo(
+    Y1names = 'island', Y2names = 'species',
+    learnt = learnt, parallel = parallel
+)
+saveRDS(temp, 'MIislandspecies.rds')
+cat('\ndone\n')
+
+temp <- mutualinfo(
+    Y1names = 'body_mass', Y2names = 'species',
+    learnt = learnt, parallel = parallel
+)
+saveRDS(temp, 'MIbodymassspecies.rds')
+cat('\ndone\n')
+
+temp <- mutualinfo(
+    Y1names = 'body_mass', Y2names = 'bill_len',
+    learnt = learnt, parallel = parallel
+)
+saveRDS(temp, 'MIbodymassbilllen.rds')
+cat('\ndone\n')
+
+temp <- mutualinfo(
+    Y1names = 'body_mass', Y2names = 'bill_len',
+    X = data.frame(species = 'Adelie'), ## choose subpopulation
+    learnt = learnt, parallel = parallel
+)
+saveRDS(temp, 'MIadelie.rds')
+cat('\ndone\n')
+
+temp <- mutualinfo(
+    Y1names = 'body_mass', Y2names = 'bill_len',
+    X = data.frame(species = 'Chinstrap'), ## choose subpopulation
+    learnt = learnt, parallel = parallel
+)
+saveRDS(temp, 'MIchinstrap.rds')
+cat('\ndone\n')
+
+temp <- mutualinfo(
+    Y1names = 'body_mass', Y2names = 'bill_len',
+    X = data.frame(species = 'Gentoo'), ## choose subpopulation
+    learnt = learnt, parallel = parallel
+)
+saveRDS(temp, 'MIgentoo.rds')
+cat('\ndone\n')
+
+
